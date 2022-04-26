99d65374d194fafa7551792cd77d860d0a2a736b
Martín <blackhack1@hotmail.com>
2022-04-26 12:41:06 -0300

    shuffle completed

--- SlidePuzzle/src/main/java/SecondApproach/GameBoard.java
+++ SlidePuzzle/src/main/java/SecondApproach/GameBoard.java
@@ -22,19 +22,14 @@ public class GameBoard {
         System.out.println("Please, type desired tile grid size (e.g. 4, to create 4x4 tile)");
         size = Integer.parseInt(sc.nextLine());
         board = new int[size][size];
-        System.out.println("\nGrid size selected: " + size + " x " + size);
-
-        StringBuilder sb = new StringBuilder(size * size * 4);
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 board[i][j] = cnt;
                 board[size-1][size-1] = hole; //Always return 0 on last index from array.
-                sb.append(String.format("%s", (board[i][j] == 0) ? "" : board[i][j])).append("\t");
                 cnt++;
             }
-            sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.println("Shuffling "+ size + " x " + size + " board\n");
         shuffle(board);
     }
 
@@ -47,56 +42,17 @@ public class GameBoard {
         }
         Collections.shuffle(list);
         cnt = 1;
+        StringBuilder sb = new StringBuilder(size * size * 4);
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 board[i][j] = list.get(cnt-1);
+                sb.append(String.format("%s", (board[i][j] == 0) ? "" : board[i][j])).append("\t");
                 cnt++;
             }
+            sb.append("\n");
         }
-        setBoard(board);
+        System.out.println(sb);
     }
-
-
-
-
-
-
-
-//    public void createBoard(){
-//        System.out.println("Please, type desired tile grid size (e.g. 4, to create 4x4 tile)");
-//        size = Integer.parseInt(sc.nextLine());
-////        int size = sel * sel;
-//        board = new int[size][size];
-//        System.out.println("\nGrid size selected: " + size + " x " + size);
-//        System.out.println(printBoard());
-//    }
-
-//    public String printBoard(){
-//        StringBuilder sb = new StringBuilder(size * size * 4);
-//        for (int i = 0; i < size; i++) {
-//            for (int j = 0; j < size; j++) {
-//                board[i][j] = cnt;
-//                board[size-1][size-1] = hole; //Always return 0 on last index from array.
-//                sb.append(String.format("%s", (board[i][j] == 0) ? "" : board[i][j])).append("\t");
-//                cnt++;
-//            }
-//            sb.append("\n");
-//        }
-//        return sb.toString();
-//    }
-
-    //Approach Joaco
-//    public static void shuffleBoard(int[][] board) {
-//        List<Integer> list = new ArrayList<>();
-//        for (int i : board) {
-//            list.add(i);
-//        }
-//        Collections.shuffle(list);
-//        for (int i = 0; i < list.size(); i++) {
-//            board[i] = list.get(i);
-//        }
-//    }
-
     public void resetBoard(){
 
     }

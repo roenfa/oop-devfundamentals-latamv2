3ad5be7934401ae4929b81f7da0d32dc3e62f011
Martín <blackhack1@hotmail.com>
2022-04-26 23:51:54 -0300

    rearranging puzzle code

--- SlidePuzzle/src/main/java/ThirdApproach/Puzzle.java
+++ SlidePuzzle/src/main/java/ThirdApproach/Puzzle.java
@@ -1,39 +1,13 @@
 package ThirdApproach;
 
 public class Puzzle {
-
     static int[][] puzzle;
     static int size, x, y, slv;
-
     public static int getSize() {
         return size;
     }
     public static void setSize(int size) {
         Puzzle.size = size;
-    }
-
-    public static boolean isSolved() {
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                if(puzzle[j][i] == 0) continue;
-                if(puzzle[j][i] != 1 + j*size + i) return false;
-                slv++;
-            }
-        }
-        return true;
-    }
-
-    public static void shufflePuzzle(int moves) {
-        boolean success = true;
-        while(moves > 0){
-            switch ((int) Math.floor(Math.random() * 4)) {
-                case 0 -> success = Movement.makeMove('W');
-                case 1 -> success = Movement.makeMove('A');
-                case 2 -> success = Movement.makeMove('S');
-                case 3 -> success = Movement.makeMove('D');
-            }
-            if(success) moves--;
-        }
     }
 
     public static void createPuzzle(int size) {
@@ -49,6 +23,19 @@ public class Puzzle {
         puzzle[x][y] = 0;
     }
 
+    public static void shufflePuzzle(int moves) {
+        boolean success = true;
+        while(moves > 0){
+            switch ((int) Math.floor(Math.random() * 4)) {
+                case 0 -> success = Movement.makeMove('W');
+                case 1 -> success = Movement.makeMove('A');
+                case 2 -> success = Movement.makeMove('S');
+                case 3 -> success = Movement.makeMove('D');
+            }
+            if(success) moves--;
+        }
+    }
+
     public static void printPuzzle() {
         StringBuilder sb = new StringBuilder(size * size * 4);
         for(int i=0; i<size; i++){
@@ -58,5 +45,16 @@ public class Puzzle {
             sb.append("\n");
         }
         System.out.println(sb);
+    }
+
+    public static boolean isSolved() {
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                if(puzzle[j][i] == 0) continue;
+                if(puzzle[j][i] != 1 + j*size + i) return false;
+                slv++;
+            }
+        }
+        return true;
     }
 }

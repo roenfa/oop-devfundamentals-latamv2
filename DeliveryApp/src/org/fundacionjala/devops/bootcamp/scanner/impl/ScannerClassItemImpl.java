e8e45fddd7421f96175691920b44d4e23b0584fd
Luis Becerra <luis.becerra.15.12@gmail.com>
2022-05-06 12:53:14 -0300

    Scanner Implementation

--- DeliveryApp/src/org/fundacionjala/devops/bootcamp/scanner/impl/ScannerClassItemImpl.java
+++ DeliveryApp/src/org/fundacionjala/devops/bootcamp/scanner/impl/ScannerClassItemImpl.java
@@ -3,19 +3,35 @@ package org.fundacionjala.devops.bootcamp.scanner.impl;
 import org.fundacionjala.devops.bootcamp.model.Item;
 import org.fundacionjala.devops.bootcamp.scanner.ScannerClass;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class ScannerClassItemImpl extends ScannerClass {
-    private Set<Item> menuSet = new TreeSet<>();
+    private Map<String, Map<String, BigDecimal>> menuSet = new HashMap<>();
 
-    public Set<Item> getMenuSet() {
-        return menuSet;
+    public getMenu() {
+        return menu;
     }
 
-    public ScannerClassItemImpl() {}
-
-    public void add(Item item){
-        menuSet.add(item);
+    public add(String name, String description, BigDecimal price) {
+        menuSet.put(name, new HashMap<String, BigDecimal>() {
+            {
+                put(description, price);
+            }
+        });
     }
+
+
+    //public Set<Item> getMenuSet() {
+    //    return menuSet;
+    //}
+
+    //public ScannerClassItemImpl() {}
+
+    //public void add(Item item){
+    //    menuSet.add(item);
+    //}
 }
